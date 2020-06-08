e82e934fb50537c7c9ba94b680a97f7884d253f2
Marcus <mojave27@gmail.com>
2020-06-07 18:34:30 -0700

    initial commit based on the AWS SDK Node Sample

---
+++ sample.js
@@ -0,0 +1,32 @@
+// Load the SDK and UUID
+var AWS = require('aws-sdk')
+var uuid = require('uuid')
+require('dotenv').config()
+
+// Create unique bucket name
+const bucketName = `node-sdk-sample-${uuid.v4()}`
+
+// Create name for uploaded object key
+var keyName = 'hello node.js'
+
+// Create a promise on S3 service object
+var bucketPromise = new AWS.S3({apiVersion: '2006-03-01'}).createBucket({Bucket: bucketName}).promise()
+
+// Handle promise fulfilled/rejected states
+bucketPromise.then(
+    function(data) {
+      // Create params for putObject call
+      var objectParams = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'};
+      // Create object upload promise
+      var uploadPromise = new AWS.S3({apiVersion: '2006-03-01'}).putObject(objectParams).promise();
+      uploadPromise.then(
+        function(data) {
+          console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
+        });
+  }).catch(
+    function(err) {
+      console.error(err, err.stack);
+  });
+
+
+
